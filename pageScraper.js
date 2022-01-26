e77c2005aa0f62fb297a3185221a20904cac6612
Werayoot kunphai <werayoot5800@gmail.com>
2022-01-26 11:27:11 +0700

    keep data from source

--- pageScraper.js
+++ pageScraper.js
@@ -1,12 +1,12 @@
 
 
 const scraperObject = {
-    url: 'https://search.drivehub.co/?booking_begin=2022-01-27%2010%3A00&booking_end=2022-01-29%2010%3A00&dealers=&from=from_homepage&location_id=205&sort_by=price',
+    url: 'https://search.drivehub.co/?booking_begin=2022-01-28%2010%3A00&booking_end=2022-01-31%2010%3A00&from=from_homepage&location_id=3&sort_by=price',
     async scraper(browser) {
         let page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url, { waitUntil: 'networkidle0' });
-
+        await page.waitForSelector(".row.car-listing");
         //Logic
         // Click all detail and keep link picture and Brand car 
         // N Scrolldown and wait network idle
